fa4d1eb3563da7bfc7479486a19ed592054f293f
DNSZLSK <DNSZLSK@users.noreply.github.com>
2025-12-30 18:37:33 +0000

    deploy: 4fb7b989bdcc9574eb1a18824cb17863159b4392

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'musubi-2025-12-30T18-37-31';
+const urlsToCache = [
+    '/musubi/',
+    '/musubi/index.html',
+    '/musubi/Music/Music1.mp3',
+    '/musubi/Music/Music2.mp3',
+    '/musubi/Music/Music3.mp3',
+    '/musubi/Music/Music4.mp3',
+    '/musubi/Music/Music5.mp3',
+    '/musubi/Music/beepMenuChoice.mp3'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    // Network first pour les assets JS/CSS, cache first pour le reste
+    if (event.request.url.includes('/assets/')) {
+        event.respondWith(
+            fetch(event.request)
+                .then((response) => {
+                    const responseClone = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => response || fetch(event.request))
+        );
+    }
+});
